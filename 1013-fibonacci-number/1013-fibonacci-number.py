a2227a53e9d776d53c085167e210ba833e65b19c
Vaibhav chauhan <117555569+vaibhav639@users.noreply.github.com>
2024-12-25 11:45:45 +0530

    Time: 41 ms (41.37%), Space: 17.6 MB (8.51%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
-        def fibo(n):
+        dp = [-1] * (n + 1)
+        def fibo(n,dp):
             if n == 0:
                 return 0
             if n == 1:
                 return 1
-            return fibo(n-1) + fibo(n-2)
-        return fibo(n)
+            if dp[n] != -1:
+                return dp[n]
+            dp[n] =  fibo(n-1,dp) + fibo(n-2,dp)
+            return dp[n]
+        return fibo(n,dp)
