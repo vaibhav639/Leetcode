e902851ac27a6010340251ed0808aef3bea07908
Vaibhav chauhan <117555569+vaibhav639@users.noreply.github.com>
2024-12-29 20:01:42 +0530

    Time: 38 ms (65.81%), Space: 40.2 MB (17.22%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
@@ -5,17 +5,29 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        lst = []
-        while head:
-            lst.append(head.val)
-            head = head.next
-        lst[k-1] , lst[len(lst)-k] = lst[len(lst)-k] , lst[k-1]
-        dummy = ListNode(0)
-        current = dummy
-        for num in lst:
-            current.next = ListNode(num)
+        # lst = []
+        # while head:
+        #     lst.append(head.val)
+        #     head = head.next
+        # lst[k-1] , lst[len(lst)-k] = lst[len(lst)-k] , lst[k-1]
+        # dummy = ListNode(0)
+        # current = dummy
+        # for num in lst:
+        #     current.next = ListNode(num)
+        #     current = current.next
+        # return dummy.next
+
+        current = head
+        for i in range(1,k):
             current = current.next
-        return dummy.next
+        a = current
+        b = head
 
-
+        while current is not None and current.next is not None:
+            current = current.next
+            b = b.next
+        temp = a.val
+        a.val = b.val
+        b.val = temp
+        return head
         
