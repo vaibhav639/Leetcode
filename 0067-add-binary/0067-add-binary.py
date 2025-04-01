78d9e3b835f087e09e95407b92961462f3e3d983
Vaibhav chauhan <117555569+vaibhav639@users.noreply.github.com>
2025-04-01 13:14:32 +0530

    Time: 4 ms (18.34%), Space: 17.7 MB (91.54%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,25 +1,11 @@
-class Solution(object):
-    def addBinary(self, a, b):
-        alen  = len(a)
-        blen = len(b)
-        carry = 0
-        ans = ""
-        i = 0
-        
-        while i < alen or i < blen or carry!=0:
-            x = 0
-            if i < alen and a[alen-i-1] == "1":
-                x = 1
-                
-            y = 0
-            if i < blen and b[blen-i-1] == "1":
-                y = 1
-                
-            ans = str((x + y + carry)%2) + ans
-            carry = (x + y + carry)//2
-            i+=1
-            
-        return ans
-                
-        
-        
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        def binary_toNum(s):
+            number = 0
+            for i in range(len(s)):
+                number += (int(s[i]) * (2**i))
+            return number
+    
+        num1 = binary_toNum(a[::-1])
+        num2 = binary_toNum(b[::-1])
+        return bin(num1+num2)[2:]
