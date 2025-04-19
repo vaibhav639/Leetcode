3bff7d4f12760f3b3b02c27abd993a41e1cc6cd9
Vaibhav chauhan <117555569+vaibhav639@users.noreply.github.com>
2025-04-19 13:06:43 +0530

    Time: 0 ms (100%), Space: 17.9 MB (40.32%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,19 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        summ = 0
-        last = 0
-        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
-        last = 0
-        for i in range(len(s)-1,-1,-1):
-            curr = dic[s[i]]
-
-            if curr < last:
-                summ -= curr
+        roman_map = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    
+        total = 0
+        prev = None
+        for char in s[::-1]:
+            if not total:
+                val = roman_map[char]
+                total+=val
+                prev = val
+            elif prev <= roman_map[char]:
+                total+= roman_map[char]
+                prev = roman_map[char]
             else:
-                summ += curr
-            last = curr
+                total-= roman_map[char]
+                prev = roman_map[char]
 
-        return summ
-
-        
+        return total
